acaab095592e8628f5136b51cf09c32fd8619be6
tonywangadong@gmail.com <tonywangadong@gmail.com@631044ad-c2e3-ffb2-0ded-e3cf29498a7f>
2012-07-13 09:26:09 +0000

    add collection bolt and reorganize the files

--- trunk/storm-dsp/src/storm/dsp/spout/RandomMessageSpout.java
+++ trunk/storm-dsp/src/storm/dsp/spout/RandomMessageSpout.java
@@ -1,5 +1,10 @@
 package storm.dsp.spout;
 
+import java.util.Map;
+import java.util.Random;
+import java.util.Vector;
+
+import storm.dsp.bolt.Message;
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
@@ -7,51 +12,44 @@ import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
-import java.util.Map;
-import java.util.Random;
-import java.util.Vector;
-
-import storm.dsp.ExclamationTopology.Message;
-
-
 
 public class RandomMessageSpout extends BaseRichSpout {
-    SpoutOutputCollector _collector;
-    Random _rand;    
-    
+	SpoutOutputCollector _collector;
+	Random _rand;
 
-    @Override
-    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-        _collector = collector;
-        _rand = new Random();
-    }
+	@Override
+	public void open(Map conf, TopologyContext context,
+			SpoutOutputCollector collector) {
+		_collector = collector;
+		_rand = new Random();
+	}
 
-    @Override
-    public void nextTuple() {
-        Utils.sleep(1000);
-        Vector<Message> msgVector = new Vector<>();
-        Message message = new Message();
-        message.setMessage("Hello World");
-        message.setStatus(Message.HELLO);
-        msgVector.add(message);
-        message.setMessage("Goodbye");
-        message.setStatus(Message.GOODBYE);
-        msgVector.add(message);
-        Message sentence = msgVector.get(_rand.nextInt(msgVector.size()));
-        _collector.emit(new Values(sentence));
-    }        
+	@Override
+	public void nextTuple() {
+		Utils.sleep(1000);
+		Vector<Message> msgVector = new Vector<>();
+		Message message = new Message();
+		message.setMessage("Hello World");
+		message.setStatus(Message.HELLO);
+		msgVector.add(message);
+		// message.setMessage("Goodbye");
+		// message.setStatus(Message.GOODBYE);
+		// msgVector.add(message);
+		Message sentence = msgVector.get(_rand.nextInt(msgVector.size()));
+		_collector.emit(new Values(message));
+	}
 
-    @Override
-    public void ack(Object id) {
-    }
+	@Override
+	public void ack(Object id) {
+	}
 
-    @Override
-    public void fail(Object id) {
-    }
+	@Override
+	public void fail(Object id) {
+	}
 
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(new Fields("word"));
-    }
-    
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("word"));
+	}
+
 }
