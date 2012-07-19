3c02f1884f9b1243f0d269242e428e25976d92e9
tonywangadong@gmail.com <tonywangadong@gmail.com@631044ad-c2e3-ffb2-0ded-e3cf29498a7f>
2012-07-19 01:46:15 +0000

    New Maven Project Framework For DSP system

---
+++ trunk/dsp-storm/src/main/java/storm/dsp/DSPTopology.java
@@ -0,0 +1,45 @@
+package storm.dsp;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import backtype.storm.utils.Utils;
+
+import storm.dsp.bolt.CollectionPoolBolt;
+import storm.dsp.bolt.Message;
+import storm.dsp.bolt.PrinterBolt;
+import storm.dsp.bolt.ProcessingBolt;
+import storm.dsp.spout.RandomMessageSpout;
+
+/**
+ * This is a basic example of a Storm topology.
+ */
+public class DSPTopology {
+
+	public static void main(String[] args) throws Exception {
+		TopologyBuilder builder = new TopologyBuilder();
+
+		builder.setSpout("source", new RandomMessageSpout(), 2);
+		builder.setBolt("collection", new CollectionPoolBolt(), 1)
+				.shuffleGrouping("source");
+		builder.setBolt("processing", new ProcessingBolt(), 1).shuffleGrouping(
+				"collection");
+		Config conf = new Config();
+		conf.setDebug(false);
+
+		if (args != null && args.length > 0) {
+			conf.setNumWorkers(3);
+			StormSubmitter.submitTopology(args[0], conf,
+					builder.createTopology());
+		} else {
+
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology("test", conf, builder.createTopology());
+			Utils.sleep(1000000);
+			cluster.killTopology("test");
+			cluster.shutdown();
+		}
+	}
+}
---
+++ trunk/dsp-storm/src/main/java/storm/dsp/bolt/ProcessingBolt.java
@@ -0,0 +1,87 @@
+package storm.dsp.bolt;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.DecisionTableConfiguration;
+import org.drools.builder.DecisionTableInputType;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+public class ProcessingBolt extends BaseRichBolt {
+	OutputCollector _collector;
+
+	@Override
+	public void prepare(Map conf, TopologyContext context,
+			OutputCollector collector) {
+		_collector = collector;
+	}
+
+	@Override
+	public void execute(Tuple tuple) {
+
+		try {
+			// load up the knowledge base
+			KnowledgeBase kbase = readKnowledgeBase();
+			StatefulKnowledgeSession ksession = kbase
+					.newStatefulKnowledgeSession();
+			KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory
+					.newFileLogger(ksession, "test");
+			// go !
+			@SuppressWarnings("unchecked")
+			ArrayList<Message> msgList=(ArrayList<Message>) tuple.getValue(0);
+			Iterator<Message> it = msgList.iterator();
+			while (it.hasNext()) {
+				Message message = (Message) it.next();
+				ksession.insert(message);
+			}
+			ksession.fireAllRules();
+			logger.close();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+
+		// _collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
+		 _collector.ack(tuple);
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+//		declarer.declare(new Fields("word"));
+	}
+	private static KnowledgeBase readKnowledgeBase() throws Exception {
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory
+				.newKnowledgeBuilder();
+		DecisionTableConfiguration config = KnowledgeBuilderFactory
+				.newDecisionTableConfiguration();
+		config.setInputType(DecisionTableInputType.XLS);
+		kbuilder.add(ResourceFactory.newClassPathResource("Sample.xls"),
+				ResourceType.DTABLE, config);
+		KnowledgeBuilderErrors errors = kbuilder.getErrors();
+		if (errors.size() > 0) {
+			for (KnowledgeBuilderError error : errors) {
+				System.err.println(error);
+			}
+			throw new IllegalArgumentException("Could not parse knowledge.");
+		}
+		KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+		return kbase;
+	}
+}
