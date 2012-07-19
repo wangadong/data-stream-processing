c47d16b25db27112bf9b75b257d36b3d67bea2cd
tonywangadong@gmail.com <tonywangadong@gmail.com@631044ad-c2e3-ffb2-0ded-e3cf29498a7f>
2012-07-19 01:46:15 +0000

    New Maven Project Framework For DSP system

---
+++ dsp-storm/src/main/java/storm/dsp/bolt/CollectionPoolBolt.java
@@ -0,0 +1,47 @@
+package storm.dsp.bolt;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class CollectionPoolBolt extends BaseRichBolt {
+	OutputCollector _collector;
+	static ArrayList msgList = new ArrayList();
+
+	@Override
+	public void prepare(Map conf, TopologyContext context,
+			OutputCollector collector) {
+		_collector = collector;
+	}
+
+	@Override
+	public void execute(Tuple tuple) {
+
+		Message message = (Message) tuple.getValue(0);
+		msgList.add(message);
+
+		// System.out.println("one message");
+		if (msgList.size() == 10) {
+			_collector.emit(tuple, new Values(new ArrayList(msgList)));
+			_collector.ack(tuple);
+			msgList.clear();
+			// System.out.println("output!");
+		}
+
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("messageList"));
+	}
+
+}
---
+++ dsp-storm/src/main/java/storm/dsp/spout/RandomMessageSpout.java
@@ -0,0 +1,55 @@
+package storm.dsp.spout;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.Vector;
+
+import storm.dsp.bolt.Message;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+public class RandomMessageSpout extends BaseRichSpout {
+	SpoutOutputCollector _collector;
+	Random _rand;
+
+	@Override
+	public void open(Map conf, TopologyContext context,
+			SpoutOutputCollector collector) {
+		_collector = collector;
+		_rand = new Random();
+	}
+
+	@Override
+	public void nextTuple() {
+		Utils.sleep(1000);
+		Vector<Message> msgVector = new Vector<>();
+		Message message = new Message();
+		message.setMessage("Hello World");
+		message.setStatus(Message.HELLO);
+		msgVector.add(message);
+		message.setMessage("Goodbye");
+		message.setStatus(Message.GOODBYE);
+		msgVector.add(message);
+		Message ranMessage = msgVector.get(_rand.nextInt(msgVector.size()));
+		_collector.emit(new Values(ranMessage));
+	}
+
+	@Override
+	public void ack(Object id) {
+	}
+
+	@Override
+	public void fail(Object id) {
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("word"));
+	}
+
+}
